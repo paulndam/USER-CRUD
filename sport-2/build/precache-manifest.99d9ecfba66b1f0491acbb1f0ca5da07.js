18ceb25c75c2a9cf80c5be7a4872073d5a8bcfcb
paulndam <59145887+paulndam@users.noreply.github.com>
2020-07-23 14:18:18 -0400

    User-Crud

---
+++ sport-2/build/precache-manifest.99d9ecfba66b1f0491acbb1f0ca5da07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3cae705f20e1e8a9c580bf43aca57f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b96a03d57196b21c536",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ab527eb1fc177751dec8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b96a03d57196b21c536",
+    "url": "/static/js/2.7e8bc9de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e8bc9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab527eb1fc177751dec8",
+    "url": "/static/js/main.cb7be4e5.chunk.js"
+  },
+  {
+    "revision": "4f9a7afa68acd4f8d1f9",
+    "url": "/static/js/runtime-main.6535d957.js"
+  }
+]);
